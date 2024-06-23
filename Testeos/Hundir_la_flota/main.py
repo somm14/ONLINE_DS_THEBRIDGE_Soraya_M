995d315b20a85057fde4899c5e1fd1364021bf58
somm14 <sorimalpica@gmail.com>
2024-06-23 14:44:23 +0200

    Create main.py

---
+++ Testeos/Hundir_la_flota/main.py
@@ -0,0 +1,14 @@
+import pruebas
+
+tablero1 = Tablero(10) 
+tablero1 = tablero1.tablero_player1(10)
+
+
+tablero2 = Tablero(10)
+tablero2.tablero_player2(10)
+
+barcos = Barco()
+
+tablero1 = barcos.coloca_barco(tablero1)
+
+tablero1
