c24b8885f336940c9c9d2e133bd6e649aca5edeb
somm14 <sorimalpica@gmail.com>
2024-06-25 05:33:17 +0200

    Update main.py

--- Testeos/Hundir_la_flota/main.py
+++ Testeos/Hundir_la_flota/main.py
@@ -1,4 +1,4 @@
-import pruebas
+from pruebas import*
 
 tablero1 = Tablero(10) 
 tablero1 = tablero1.tablero_player1(10)
@@ -11,4 +11,31 @@ barcos = Barco()
 
 tablero1 = barcos.coloca_barco(tablero1)
 
-tablero1
+tablero2 = Tablero(10)
+tablero2.tablero_player2(10)
+
+barcos_player2 = [
+    Barco("barco1", 1),
+    Barco("barco2", 1),
+    Barco("barco3", 1),
+    Barco("barco4", 1),
+    Barco("barco5", 2),
+    Barco("barco6", 2),
+    Barco("barco7", 2),
+    Barco("barco8", 3),
+    Barco("barco9", 3),
+    Barco("barco10", 4)
+]
+
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco1")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco2")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco3")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco4")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco5")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco6")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco7")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco8")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco9")
+tablero2 = barcos_player2.crea_barco_aleatorio(tablero2, "barco10")
+
+print(tablero2)
