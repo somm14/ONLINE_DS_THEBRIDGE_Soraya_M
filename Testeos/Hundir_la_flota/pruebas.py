f3d85189cb5946253177ba152ed5a8d91b3a5c85
somm14 <sorimalpica@gmail.com>
2024-06-25 05:33:19 +0200

    Update pruebas.py

--- Testeos/Hundir_la_flota/pruebas.py
+++ Testeos/Hundir_la_flota/pruebas.py
@@ -1,16 +1,17 @@
 import numpy as np
-import main
+import random
+from main import*
 
-class Tablero():
-    def __init__(self, tamaño = 10):
+class Tablero:
+    def __init__(self, tamaño=10):
         self.tamaño = tamaño
-        
+
     def tablero_player1(self, tamaño):
-        tablero1 = np.full((tamaño,tamaño), " ")
+        tablero1 = np.full((tamaño, tamaño), " ")
         return tablero1
-    
+
     def tablero_player2(self, tamaño):
-        tablero2 = np.full((tamaño,tamaño), " ")
+        tablero2 = np.full((tamaño, tamaño), " ")
         return tablero2
 
 class Barco:
@@ -25,3 +26,11 @@ class Barco:
             for pieza in barco:
                 tablero[pieza] = "O"
         return tablero
+    
+    def crea_barco_aleatorio(self, tablero, nombre):
+        longitud = nombre.longitud
+        fila = random.randint(0,9)
+        columna = random.randint(0,9)
+        orientacion = random.choice(["N", "S", "E", "O"])
+        if orientacion == "N": 
+            if fila - longitud >= 0: 
